e9256f53d0bb49785f8b757ee1659f0f8cf4fbe2
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 18:43:10 -0300

    Add files via upload

---
+++ listaPsc02_12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class listaPsc02_12 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe um número inteiro de 1 a 10: ");
+        int N = teclado.nextInt();
+        teclado.close();
+     
+        System.out.println(">> Tabuada de multiplicação do " + N + ":");
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        System.out.println(">> Tabuada de adição de " + N + ":");       
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(N + " + " + i + " = " + (N + i));           
+        }
+
+        System.out.println(">> Tabuada de subtração do " + N + ":");
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(N + " - " + i + " = " + (N - i));
+     
+        }
+
+    }
+}
