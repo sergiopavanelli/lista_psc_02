052c6fa6bbbde396493940334fb7ba5977b30106
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 19:03:38 -0300

    Add files via upload

---
+++ listaPsc02_13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class listaPsc02_13 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Informe o primeiro número (base): ");
+        double base = teclado.nextDouble();
+
+        System.out.println("Informe o segundo número (expoente): ");
+        double expoente = teclado.nextDouble();
+
+        teclado.close();
+
+        double resultado = Math.pow(base, expoente);
+
+        System.out.println("O primeiro número (base) elevado ao segundo número (expoente) é: " + resultado);
+
+    }
+}
