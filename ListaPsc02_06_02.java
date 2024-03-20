262da1468190ab2c751f63cef7e31f66108d002f
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 15:48:22 -0300

    Add files via upload

---
+++ ListaPsc02_06_02.java
@@ -0,0 +1,15 @@
+public class ListaPsc02_06_02 {
+    
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 20 ) {
+
+            System.out.print(i + " ");
+
+            i +=1;
+    
+        }
+    }
+}
