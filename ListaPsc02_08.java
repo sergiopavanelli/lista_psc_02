999edee89ac3614506befa889a79fcc1041fe877
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 16:38:33 -0300

    Add files via upload

---
+++ ListaPsc02_08.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ListaPsc02_08 {
+    public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+
+    float soma = 0;
+   
+    for (int i = 0; i < 5; i++) {
+
+        System.out.println("Digite o número " + (i + 1) + ": ");
+        float numero = teclado.nextFloat();
+
+        soma = numero + soma;
+                
+        }
+    
+    float media = soma / 5;
+
+    System.out.println("A soma dos 5 números digitados é: " + soma);
+    System.out.println("A media dos 5 números digitados é: " + media);
+
+    }
+}
