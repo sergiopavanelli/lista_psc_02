78794f5789c5935670d36cec2f848b9f5974b1dd
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 09:28:10 -0300

    Update and rename lista psc_09.java to ListaPsc02_02.java

---
+++ ListaPsc02_02.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ListaPsc02_02 {
+    public static void main(String[] args) {
+
+Scanner teclado = new Scanner(System.in);
+
+String nome;
+String senha;
+
+do {
+
+    System.out.println("Digite o nome de usuário:");
+    nome = teclado.nextLine();
+    System.out.println("Digite a senha:");
+    senha = teclado.nextLine();
+
+    if (nome.equals(senha)) {
+
+    System.out.println("A senha não pode ser igual ao nome do usário!");
+
+}
+
+} while (nome.equals(senha));
+
+teclado.close();
+
+    System.out.println("Usuário e senha cadastrados com sucesso.");
+
+}
+}
