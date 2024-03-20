6f6af6d7f5a3f575dcc504b2bb238ac64fa10df8
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 09:26:39 -0300

    Add files via upload

---
+++ ListaPsc02_01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ListaPsc02_01 {
+    public static void main(String[] args) {
+
+Scanner teclado = new Scanner(System.in);
+double nota;
+
+do {
+    System.out.println("Informe uma nota entre zero e dez:");
+    nota = teclado.nextDouble();
+
+    if (nota < 0 || nota > 10) {
+        System.out.println("Nota inválida! A nota deve ser de zero a dez!");
+    }
+} while (nota < 0 || nota > 10);
+
+    System.out.println("Nota válida.");
+    teclado.close();
+
+    }
+}
