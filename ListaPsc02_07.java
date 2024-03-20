ea1abe5a6a0fa4550360c46d1b49a8a02233979e
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-20 16:09:47 -0300

    Add files via upload

---
+++ ListaPsc02_07.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ListaPsc02_07 {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+        float maior = Float.MIN_VALUE;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite o número " + (i+1) + ": ");
+            float numero = teclado.nextFloat();
+      
+            if (numero > maior) {
+                maior = numero;
+            }
+        }
+
+        System.out.println("O maior número digitado foi: " + maior);
+        teclado.close();
+
+    }      
+
+}
