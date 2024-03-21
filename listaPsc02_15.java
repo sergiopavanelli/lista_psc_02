0cb3260a5d2afba938187a456b676e4ffa806f93
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-21 09:30:06 -0300

    Update listaPsc02_15.java

--- listaPsc02_15.java
+++ listaPsc02_15.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 public class listaPsc02_15 {
     
     public static void main(String[] args) {
         
-
         int n1 = 0;
         int n2 = 1;
         int nextNumber;
+        int n;
 
-        for (int i = 1; i <= 15; i++) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite o número de termos desejado na sequência de Fibonacci: ");
+        n = teclado.nextInt();
+        teclado.close();
+
+
+        for (int i = 1; i <= n; i++) {
            
             System.out.println(n1);
 
