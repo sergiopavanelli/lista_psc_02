05012267f8d146be166f3cd5521bfd714ce781ac
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-21 18:21:12 -0300

    Add files via upload

---
+++ listaPsc02_17.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class listaPsc02_17 {
+
+    public static void main(String[] args) {
+
+        int n;
+        int fatorial = 1;
+       
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        n = teclado.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+
+            fatorial *= i;
+        
+        }
+
+        teclado.close();
+        
+        System.out.println("O valor de " + n + "! é: " + fatorial);
+    
+    }
+}
