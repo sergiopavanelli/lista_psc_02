fee4530a6e709828ada5da74b38fdbe0a27b9668
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-22 20:13:26 -0300

    Add files via upload

---
+++ listaPsc02_32.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class listaPsc02_32 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro para calcular o fatorial: ");
+        int numero = scanner.nextInt();
+
+        long fatorial = calcularFatorial(numero);
+
+        System.out.println("Fatorial de " + numero + ":");
+        System.out.print(numero + "! = ");
+        for (int i = numero; i >= 1; i--) {
+            System.out.print(i);
+            if (i > 1) {
+                System.out.print(" * ");
+            }
+        }
+        System.out.println(" = " + fatorial);
+
+        scanner.close();
+    }
+
+    public static long calcularFatorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * calcularFatorial(n - 1);
+        }
+    }
+}
