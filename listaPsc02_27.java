09016020ec72cfb1b69f818b060a6d7d1f68c18b
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-22 18:11:51 -0300

    Add files via upload

---
+++ listaPsc02_27.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class listaPsc02_27 {
+    
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int turmas;
+        int alunos;
+        int soma_alunos = 0;
+
+        System.out.println("Digite a quantidade de turmas:");
+        turmas = teclado.nextInt();
+
+        for (int i = 1; i <= turmas; i++) {
+            System.out.println("Digite a quantidade de alunos na turma: " + i);
+            alunos = teclado.nextInt();
+            if (alunos > 40) {
+                System.out.println("A turma não pode ter mais de 40 alunos");
+                i--; // volta para o loop           
+            } else { 
+                soma_alunos += alunos;
+            }
+        }
+
+        System.out.println("A média de alunos por turma é " + (soma_alunos / turmas));
+        teclado.close();
+    }
+}
