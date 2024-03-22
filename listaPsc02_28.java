aae2c0edb5af0d968f3127cf8f294d322bd783d2
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-22 18:23:36 -0300

    Add files via upload

---
+++ listaPsc02_28.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class listaPsc02_28 {
+
+    public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+
+    int quant_cds;
+    float valor_total_cds = 0;
+
+    System.out.println("Digite a quantidade de CD's na coleção:");
+    quant_cds = teclado.nextInt();
+
+
+    for (int i = 1; i <= quant_cds; i++) {
+
+        System.out.println("Digite o valor médio em R$ gasto no " + i + "º CD:");
+        valor_total_cds += teclado.nextFloat();
+    }
+
+    System.out.println("O valor total investido pelo colecionar em sua coleção de CD's é: R$ " + valor_total_cds);
+    System.out.println("O valor médio gasto em cada CD é: R$ " + (valor_total_cds / quant_cds));
+
+    teclado.close();
+
+    }
+    
+}
