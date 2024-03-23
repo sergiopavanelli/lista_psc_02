bd81461fb83e93a4bbbd1c3632cf197170f3fb85
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-23 16:02:20 -0300

    Add files via upload

---
+++ listaPsc02_34.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class listaPsc02_34 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int N;
+
+        System.out.println("Digite um número inteiro: ");
+        N = teclado.nextInt();
+
+        boolean primo = true;
+
+        if (N <= 1) {
+            primo = false;
+        } else {
+            for (int i = 2; i < N; i++) {
+                if (N % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+        }
+
+        if (primo) {
+            System.out.println("O número " + N + " é primo.");
+        } else {
+            System.out.println("O número " + N + " não é primo.");
+        }
+        
+        teclado.close();
+
+    }
+    
+}
