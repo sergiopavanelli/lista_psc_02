7634bfb972b5c93f6f8e886f21ec07c7a2f5d4a3
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-23 18:12:53 -0300

    Add files via upload

---
+++ listaPsc02_39.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class listaPsc02_39 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        double altura;
+        double maior_altura = Double.MIN_VALUE;
+        double menor_altura = Double.MAX_VALUE;
+        int numero_maior_aluno = 0;
+        int numero_menor_aluno = 0;
+        
+        
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println("Digite a altura do " + i + "º aluno em metros:");
+            altura = teclado.nextDouble();
+
+            if (altura > maior_altura) {
+                maior_altura = altura;
+                numero_maior_aluno = i;
+            }
+            if (altura < menor_altura) {
+                menor_altura = altura;
+                numero_menor_aluno = i;  
+            }
+
+        }
+
+        teclado.close();
+
+        System.out.println("Aluno com maior altura: " + numero_maior_aluno + "° >> " + maior_altura + "m");
+        System.out.println("Aluno com menor altura: " + numero_menor_aluno + "° >> " + menor_altura + "m");
+    }
+}
