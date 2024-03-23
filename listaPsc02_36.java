8fbf22ddf39acf10c594abaa74fc9ac6b01f5f94
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-23 16:21:51 -0300

    Add files via upload

---
+++ listaPsc02_36.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class listaPsc02_36 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int N;
+        int nInicial;
+        int nFinal;
+
+
+        System.out.println("Informe um número inteiro para montagem da tabuada: ");
+        N = teclado.nextInt();
+
+        System.out.println("Informe o primeiro número da tabuada: ");
+        nInicial = teclado.nextInt();
+
+        System.out.println("Informe o último número da tabuada: ");
+        nFinal = teclado.nextInt();
+  
+     
+        System.out.println(" Vou montar a tabuada de " + N + " começando em " + nInicial + " e terminando em " + nFinal + ":");
+
+        for (int i = nInicial; i <= nFinal; i++) {
+
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        teclado.close();
+    }
+    
+}
