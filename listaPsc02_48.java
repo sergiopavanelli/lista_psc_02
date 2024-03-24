959997458c95a9c7e0570996748baa74b18371ca
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-24 13:04:08 -0300

    Add files via upload

---
+++ listaPsc02_48.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class listaPsc02_48 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int number = teclado.nextInt();
+        
+
+        if (number < 0) {
+            System.out.println("O número digitado não é positivo.");
+            teclado.close();
+            return;
+            
+        }
+
+        int reversedNumber = reverseNumber(number);
+       
+
+        System.out.println("Número invertido: " + reversedNumber);
+
+        teclado.close();
+
+        }
+
+
+    
+    public static int reverseNumber(int num) {
+        int reversedNum = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        return reversedNum;
+
+    }
+
+
+}
