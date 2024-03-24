dbcdaae482a8201b2e6218cd1543ebf576766a5e
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-03-24 13:45:38 -0300

    Add files via upload

---
+++ listaPsc02_51.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class listaPsc02_51 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Digite o valor de n: ");
+        int n = teclado.nextInt();
+        
+        double soma = 0;
+        int numerador = 1;
+        int denominador = 1;
+        
+        System.out.print("S = ");
+        for (int i = 1; i <= n; i++) {
+            soma += (double) numerador / denominador;
+            
+            if (i != 1)
+                System.out.print(" + ");
+            
+            System.out.print(numerador + "/" + denominador);
+            
+            numerador++;
+            denominador += 2;
+        }
+        
+        System.out.println("\n\nA soma da série é: " + soma);
+        
+        teclado.close();
+   
+    }
+}
